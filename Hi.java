2a8d07bdef08c96a6e4a700daa3b9b10028f13aa
nigthblue <user@>
2020-11-05 01:11:46 +0300

    first commit

---
+++ Hi.java
@@ -0,0 +1,18 @@
+
+public class Hi{
+
+    public static void main(String arg[]){
+        extracted();
+        
+    }
+
+    private static void extracted() {
+        System.currentTimeMillis();
+        String x = "Hi";
+        System.out.println(x);
+        System.currentTimeMillis();
+    }
+    private static void ex(){
+
+    }
+}
